ac25f77a95d6a7c2187ce8f42c0633f1bc330409
WeiZhang <cyoi0129@gmail.com>
2021-10-29 23:56:14 +0900

    Change origin policy

--- project/settings.py
+++ project/settings.py
@@ -64,13 +64,14 @@ TEMPLATES = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-           'rest_framework.authentication.TokenAuthentication',
-           'rest_framework.authentication.BasicAuthentication',
-       ),
-       'DEFAULT_PERMISSION_CLASSES': (
-            'rest_framework.permissions.AllowAny',
-       ),
-  }
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+}
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
